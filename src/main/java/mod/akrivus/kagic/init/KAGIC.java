5eb8fff6e82b8bfba7d9ffda4d66bbe47fb6bc30
gryttr <heimrarnardalr@gmail.com>
2017-09-02 16:24:18 -0400

    Updated version number

--- src/main/java/mod/akrivus/kagic/init/KAGIC.java
+++ src/main/java/mod/akrivus/kagic/init/KAGIC.java
@@ -32,7 +32,7 @@ import net.minecraftforge.fml.relauncher.Side;
 @Mod(modid = KAGIC.MODID, version = KAGIC.VERSION, acceptedMinecraftVersions = KAGIC.MCVERSION, guiFactory = "mod.akrivus.kagic.client.gui.GuiFactory")
 public class KAGIC {
     public static final String MODID = "kagic";
-    public static final String VERSION = "1.10";
+    public static final String VERSION = "1.10.0-a1";
     public static final String MCVERSION = "[1.12,1.12.1]";
     public static final boolean DEVELOPER = true;
  
