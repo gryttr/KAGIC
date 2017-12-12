5d852d55d27f25fe470442165391f2c7732d787e
gryttr <heimrarnardalr@gmail.com>
2017-12-11 20:49:08 -0500

    Fixed incubators not dropping gem bases when broken

--- src/main/java/mod/akrivus/kagic/blocks/BlockIncubator.java
+++ src/main/java/mod/akrivus/kagic/blocks/BlockIncubator.java
@@ -15,6 +15,7 @@ import net.minecraft.block.state.BlockStateContainer;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.inventory.InventoryHelper;
 import net.minecraft.item.ItemStack;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.BlockRenderLayer;
@@ -31,6 +32,7 @@ import net.minecraftforge.fml.relauncher.SideOnly;
 
 public class BlockIncubator extends BlockContainer {
 	public static final PropertyDirection FACING = BlockHorizontal.FACING;
+	
 	public BlockIncubator() {
 		super(Material.GLASS, MapColor.MAGENTA);
 		this.setUnlocalizedName("incubator");
@@ -38,103 +40,141 @@ public class BlockIncubator extends BlockContainer {
 		this.setDefaultState(this.blockState.getBaseState().withProperty(FACING, EnumFacing.NORTH));
 		this.setLightLevel(1.0F);
 	}
+
+	@Override
 	public void onBlockAdded(World worldIn, BlockPos pos, IBlockState state) {
-        super.onBlockAdded(worldIn, pos, state);
+		super.onBlockAdded(worldIn, pos, state);
 		this.setDefaultFacing(worldIn, pos, state);
-    }
+	}
 	public IBlockState onBlockPlaced(World worldIn, BlockPos pos, EnumFacing facing, float hitX, float hitY, float hitZ, int meta, EntityLivingBase placer) {
-        return this.getDefaultState().withProperty(FACING, placer.getHorizontalFacing().getOpposite());
-    }
-    public void onBlockPlacedBy(World worldIn, BlockPos pos, IBlockState state, EntityLivingBase placer, ItemStack stack) {
-        worldIn.setBlockState(pos, state.withProperty(FACING, placer.getHorizontalFacing().getOpposite()), 2);
-        Injector.onIncubatorPlacement(worldIn, pos);
-    }
-    public void neighborChanged(IBlockState state, World worldIn, BlockPos pos, Block blockIn, BlockPos p_189540_5_) {
+		return this.getDefaultState().withProperty(FACING, placer.getHorizontalFacing().getOpposite());
+	}
+
+	@Override
+	public void onBlockPlacedBy(World worldIn, BlockPos pos, IBlockState state, EntityLivingBase placer, ItemStack stack) {
+		worldIn.setBlockState(pos, state.withProperty(FACING, placer.getHorizontalFacing().getOpposite()), 2);
+		Injector.onIncubatorPlacement(worldIn, pos);
+	}
+
+	@Override
+	public void neighborChanged(IBlockState state, World worldIn, BlockPos pos, Block blockIn, BlockPos p_189540_5_) {
 		IBlockState below = worldIn.getBlockState(pos.down());
-    	if (below.getBlock() instanceof BlockInjector) {
-    		worldIn.setBlockState(pos, state.withProperty(FACING, (EnumFacing) below.getProperties().get(BlockInjector.FACING)), 2);
-    	}
-    }
-    public boolean onBlockActivated(World worldIn, BlockPos pos, IBlockState state, EntityPlayer playerIn, EnumHand hand, EnumFacing facing, float hitX, float hitY, float hitZ) {
-        TileEntity container = worldIn.getTileEntity(pos);
-        if (container != null && container instanceof TileEntityIncubator) {
-        	TileEntityIncubator incubator = (TileEntityIncubator) container;
-            playerIn.displayGUIChest(incubator);
-        }
-        return true;
-    }
-    /*********************************************************
-     * Methods related to block states and direction.        *
-     *********************************************************/
-    private void setDefaultFacing(World worldIn, BlockPos pos, IBlockState state) {
-        if (!worldIn.isRemote) {
-            IBlockState iblockstate = worldIn.getBlockState(pos.north());
-            IBlockState iblockstate1 = worldIn.getBlockState(pos.south());
-            IBlockState iblockstate2 = worldIn.getBlockState(pos.west());
-            IBlockState iblockstate3 = worldIn.getBlockState(pos.east());
-            EnumFacing enumfacing = (EnumFacing)state.getValue(FACING);
-            if (enumfacing == EnumFacing.NORTH && iblockstate.isFullBlock() && !iblockstate1.isFullBlock()) {
-                enumfacing = EnumFacing.SOUTH;
-            }
-            else if (enumfacing == EnumFacing.SOUTH && iblockstate1.isFullBlock() && !iblockstate.isFullBlock()) {
-                enumfacing = EnumFacing.NORTH;
-            }
-            else if (enumfacing == EnumFacing.WEST && iblockstate2.isFullBlock() && !iblockstate3.isFullBlock()) {
-                enumfacing = EnumFacing.EAST;
-            }
-            else if (enumfacing == EnumFacing.EAST && iblockstate3.isFullBlock() && !iblockstate2.isFullBlock()) {
-                enumfacing = EnumFacing.WEST;
-            }
-            worldIn.setBlockState(pos, state.withProperty(FACING, enumfacing), 2);
-        }
-    }
-    public static void setState(boolean active, World worldIn, BlockPos pos) {
-        IBlockState iblockstate = worldIn.getBlockState(pos);
-        worldIn.setBlockState(pos, ModBlocks.INCUBATOR.getDefaultState().withProperty(FACING, iblockstate.getValue(FACING)), 3);
-        worldIn.setBlockState(pos, ModBlocks.INCUBATOR.getDefaultState().withProperty(FACING, iblockstate.getValue(FACING)), 3);
-    }
-    public IBlockState getStateFromMeta(int meta) {
-        EnumFacing enumfacing = EnumFacing.getFront(meta);
-        if (enumfacing.getAxis() == EnumFacing.Axis.Y) {
-            enumfacing = EnumFacing.NORTH;
-        }
-        return this.getDefaultState().withProperty(FACING, enumfacing);
-    }
-    public int getMetaFromState(IBlockState state) {
-        return ((EnumFacing)state.getValue(FACING)).getIndex();
-    }
-    public IBlockState withRotation(IBlockState state, Rotation rot) {
-        return state.withProperty(FACING, rot.rotate((EnumFacing)state.getValue(FACING)));
-    }
-    public IBlockState withMirror(IBlockState state, Mirror mirrorIn) {
-        return state.withRotation(mirrorIn.toRotation((EnumFacing)state.getValue(FACING)));
-    }
-    protected BlockStateContainer createBlockState() {
-        return new BlockStateContainer(this, new IProperty[] {FACING});
-    }
+		if (below.getBlock() instanceof BlockInjector) {
+			worldIn.setBlockState(pos, state.withProperty(FACING, (EnumFacing) below.getProperties().get(BlockInjector.FACING)), 2);
+		}
+	}
+
+	@Override
+	public boolean onBlockActivated(World worldIn, BlockPos pos, IBlockState state, EntityPlayer playerIn, EnumHand hand, EnumFacing facing, float hitX, float hitY, float hitZ) {
+		TileEntity container = worldIn.getTileEntity(pos);
+		if (container != null && container instanceof TileEntityIncubator) {
+			TileEntityIncubator incubator = (TileEntityIncubator) container;
+			playerIn.displayGUIChest(incubator);
+		}
+		return true;
+	}
+
+	@Override
+	public void breakBlock(World world, BlockPos pos, IBlockState state) {
+		TileEntity incInv = world.getTileEntity(pos);
+		if (incInv instanceof TileEntityIncubator) {
+			InventoryHelper.dropInventoryItems(world, pos, (TileEntityIncubator)incInv);
+		}
+	}
+	
+	/*********************************************************
+	 * Methods related to block states and direction.		*
+	 *********************************************************/
+	private void setDefaultFacing(World worldIn, BlockPos pos, IBlockState state) {
+		if (!worldIn.isRemote) {
+			IBlockState iblockstate = worldIn.getBlockState(pos.north());
+			IBlockState iblockstate1 = worldIn.getBlockState(pos.south());
+			IBlockState iblockstate2 = worldIn.getBlockState(pos.west());
+			IBlockState iblockstate3 = worldIn.getBlockState(pos.east());
+			EnumFacing enumfacing = (EnumFacing)state.getValue(FACING);
+			if (enumfacing == EnumFacing.NORTH && iblockstate.isFullBlock() && !iblockstate1.isFullBlock()) {
+				enumfacing = EnumFacing.SOUTH;
+			}
+			else if (enumfacing == EnumFacing.SOUTH && iblockstate1.isFullBlock() && !iblockstate.isFullBlock()) {
+				enumfacing = EnumFacing.NORTH;
+			}
+			else if (enumfacing == EnumFacing.WEST && iblockstate2.isFullBlock() && !iblockstate3.isFullBlock()) {
+				enumfacing = EnumFacing.EAST;
+			}
+			else if (enumfacing == EnumFacing.EAST && iblockstate3.isFullBlock() && !iblockstate2.isFullBlock()) {
+				enumfacing = EnumFacing.WEST;
+			}
+			worldIn.setBlockState(pos, state.withProperty(FACING, enumfacing), 2);
+		}
+	}
+	public static void setState(boolean active, World worldIn, BlockPos pos) {
+		IBlockState iblockstate = worldIn.getBlockState(pos);
+		worldIn.setBlockState(pos, ModBlocks.INCUBATOR.getDefaultState().withProperty(FACING, iblockstate.getValue(FACING)), 3);
+		worldIn.setBlockState(pos, ModBlocks.INCUBATOR.getDefaultState().withProperty(FACING, iblockstate.getValue(FACING)), 3);
+	}
+
+	@Override
+	public IBlockState getStateFromMeta(int meta) {
+		EnumFacing enumfacing = EnumFacing.getFront(meta);
+		if (enumfacing.getAxis() == EnumFacing.Axis.Y) {
+			enumfacing = EnumFacing.NORTH;
+		}
+		return this.getDefaultState().withProperty(FACING, enumfacing);
+	}
+
+	@Override
+	public int getMetaFromState(IBlockState state) {
+		return ((EnumFacing)state.getValue(FACING)).getIndex();
+	}
+
+	@Override
+	public IBlockState withRotation(IBlockState state, Rotation rot) {
+		return state.withProperty(FACING, rot.rotate((EnumFacing)state.getValue(FACING)));
+	}
+
+	@Override
+	public IBlockState withMirror(IBlockState state, Mirror mirrorIn) {
+		return state.withRotation(mirrorIn.toRotation((EnumFacing)state.getValue(FACING)));
+	}
+
+	@Override
+	protected BlockStateContainer createBlockState() {
+		return new BlockStateContainer(this, new IProperty[] {FACING});
+	}
+
+	@Override
 	public TileEntity createNewTileEntity(World worldIn, int meta) {
 		return new TileEntityIncubator();
 	}
-    
-    /*********************************************************
-     * Methods related to block rendering.                   *
-     *********************************************************/
-    @SideOnly(Side.CLIENT)
-    public BlockRenderLayer getBlockLayer() {
-        return BlockRenderLayer.TRANSLUCENT;
-    }
-    @SideOnly(Side.CLIENT)
-    public EnumBlockRenderType getRenderType(IBlockState state) {
-        return EnumBlockRenderType.MODEL;
-    }
-    @SideOnly(Side.CLIENT)
-    public boolean shouldSideBeRendered(IBlockState blockState, IBlockAccess blockAccess, BlockPos pos, EnumFacing side) {
-        return true;
-    }
-    public boolean isFullCube(IBlockState state) {
-        return false;
-    }
-    public boolean isOpaqueCube(IBlockState state) {
-        return false;
-    }
+	
+	/*********************************************************
+	 * Methods related to block rendering.				   *
+	 *********************************************************/
+	@Override
+	@SideOnly(Side.CLIENT)
+	public BlockRenderLayer getBlockLayer() {
+		return BlockRenderLayer.TRANSLUCENT;
+	}
+
+	@Override
+	@SideOnly(Side.CLIENT)
+	public EnumBlockRenderType getRenderType(IBlockState state) {
+		return EnumBlockRenderType.MODEL;
+	}
+
+	@Override
+	@SideOnly(Side.CLIENT)
+	public boolean shouldSideBeRendered(IBlockState blockState, IBlockAccess blockAccess, BlockPos pos, EnumFacing side) {
+		return true;
+	}
+
+	@Override
+	public boolean isFullCube(IBlockState state) {
+		return false;
+	}
+
+	@Override
+	public boolean isOpaqueCube(IBlockState state) {
+		return false;
+	}
 }
--- src/main/java/mod/akrivus/kagic/tileentity/TileEntityIncubator.java
+++ src/main/java/mod/akrivus/kagic/tileentity/TileEntityIncubator.java
@@ -8,6 +8,7 @@ import mod.akrivus.kagic.util.injector.Incubator;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.entity.player.InventoryPlayer;
 import net.minecraft.inventory.Container;
+import net.minecraft.inventory.IInventory;
 import net.minecraft.inventory.ItemStackHelper;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
@@ -19,56 +20,76 @@ import net.minecraft.util.text.TextComponentTranslation;
 
 public class TileEntityIncubator extends TileEntityLockableLoot implements ITickable {
 	private NonNullList<ItemStack> chestContents = NonNullList.<ItemStack>withSize(this.getSizeInventory(), ItemStack.EMPTY);
+	
+	@Override
 	public void readFromNBT(NBTTagCompound compound) {
-        super.readFromNBT(compound);
-        this.chestContents = NonNullList.<ItemStack>withSize(this.getSizeInventory(), ItemStack.EMPTY);
-        if (!this.checkLootAndRead(compound)) {
-            ItemStackHelper.loadAllItems(compound, this.chestContents);
-        }
-    }
-    public NBTTagCompound writeToNBT(NBTTagCompound compound) {
-        super.writeToNBT(compound);
-        if (!this.checkLootAndWrite(compound)) {
-            ItemStackHelper.saveAllItems(compound, this.chestContents);
-        }
-        return compound;
-    }
-    public void update() {
-    	if (this.world.getBlockState(this.pos.down()).getBlock() == ModBlocks.INJECTOR && !this.world.isBlockPowered(this.pos.down())) {
-    		boolean containsBase = false;
-    		for (int i = 0; i < this.chestContents.size(); ++i) {
-    			ItemStack stack = this.chestContents.get(i);
-    			if (stack.getItem() == ModItems.ACTIVATED_GEM_BASE) {
-    				stack.shrink(1);
-    				this.chestContents.set(i, stack);
-    				containsBase = true;
-    				break;
-    			}
-    		}
-    		if (containsBase) {
-    			this.world.setBlockState(this.pos.down(), ModBlocks.EQUIPPED_INJECTOR.getDefaultState().withProperty(BlockInjector.FACING, this.world.getBlockState(this.pos.down()).getValue(BlockInjector.FACING)));
-    			this.world.playSound(null, this.pos.down(), ModSounds.BLOCK_INJECTOR_CLOSE, SoundCategory.BLOCKS, 0.3F, 1.0F);
-    		}
-    	}
-    }
+		super.readFromNBT(compound);
+		this.chestContents = NonNullList.<ItemStack>withSize(this.getSizeInventory(), ItemStack.EMPTY);
+		if (!this.checkLootAndRead(compound)) {
+			ItemStackHelper.loadAllItems(compound, this.chestContents);
+		}
+	}
+	
+	@Override
+	public NBTTagCompound writeToNBT(NBTTagCompound compound) {
+		super.writeToNBT(compound);
+		if (!this.checkLootAndWrite(compound)) {
+			ItemStackHelper.saveAllItems(compound, this.chestContents);
+		}
+		return compound;
+	}
+	
+	@Override
+	public void update() {
+		if (this.world.getBlockState(this.pos.down()).getBlock() == ModBlocks.INJECTOR && !this.world.isBlockPowered(this.pos.down())) {
+			boolean containsBase = false;
+			for (int i = 0; i < this.chestContents.size(); ++i) {
+				ItemStack stack = this.chestContents.get(i);
+				if (stack.getItem() == ModItems.ACTIVATED_GEM_BASE) {
+					stack.shrink(1);
+					this.chestContents.set(i, stack);
+					containsBase = true;
+					break;
+				}
+			}
+			if (containsBase) {
+				this.world.setBlockState(this.pos.down(), ModBlocks.EQUIPPED_INJECTOR.getDefaultState().withProperty(BlockInjector.FACING, this.world.getBlockState(this.pos.down()).getValue(BlockInjector.FACING)));
+				this.world.playSound(null, this.pos.down(), ModSounds.BLOCK_INJECTOR_CLOSE, SoundCategory.BLOCKS, 0.3F, 1.0F);
+			}
+		}
+	}
+	
+	@Override
 	public int getSizeInventory() {
 		return 9;
 	}
+	
+	@Override
 	public boolean isEmpty() {
 		return false;
 	}
+	
+	@Override
 	public int getInventoryStackLimit() {
 		return 64;
 	}
+	
+	@Override
 	public String getName() {
 		return new TextComponentTranslation("tile.incubator.name").getUnformattedText();
 	}
+	
+	@Override
 	public Container createContainer(InventoryPlayer playerInventory, EntityPlayer playerIn) {
-        return new Incubator(playerInventory, this);
+		return new Incubator(playerInventory, this);
 	}
+	
+	@Override
 	public String getGuiID() {
 		return "kagic:incubator";
 	}
+	
+	@Override
 	protected NonNullList<ItemStack> getItems() {
 		return this.chestContents;
 	}
