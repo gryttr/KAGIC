892c45c1ef5d91c045fffc284efc809b48bd5d33
gryttr <heimrarnardalr@gmail.com>
2018-01-27 13:28:21 -0500

    Fixed peridot not reporting gem quality when clicked with an empty hand

--- src/main/java/mod/akrivus/kagic/entity/gem/EntityPeridot.java
+++ src/main/java/mod/akrivus/kagic/entity/gem/EntityPeridot.java
@@ -213,7 +213,7 @@ public class EntityPeridot extends EntityGem implements IInventoryChangedListene
 						if (this.isCoreItem(stack)) {
 							return super.processInteract(player, hand);
 						}
-						else if (stack.getItem() instanceof ItemHoe || stack.isEmpty()) {
+						else if (stack.getItem() instanceof ItemHoe || (stack.isEmpty() && this.isFarmer())) {
 							boolean toolChanged = true;
 							if (!this.getItemStackFromSlot(EntityEquipmentSlot.MAINHAND).isItemEqualIgnoreDurability(stack)) {
 								this.entityDropItem(this.getItemStackFromSlot(EntityEquipmentSlot.MAINHAND), 0.0F);
